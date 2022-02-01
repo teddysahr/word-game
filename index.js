7975539b45629a45034b3ee3173e3ba538d7c7cb
Teddy Sahr <tedward1218@gmail.com>
2022-02-01 17:57:21 -0500

    objective words display on start

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const chosenWordDisplay = document.getElementById("objective-words");
+const startButton = document.getElementById("start-button");
+
 const wordList = [
   ["ROBOT", "WINCE"],
   ["POWER", "GUILD"],
 ];
+
+function runGame() {
+  chosenWordDisplay.textContent = getWords();
+}
+
+function getWords() {
+  return wordList[0];
+}
+
+startButton.addEventListener("click", runGame);
