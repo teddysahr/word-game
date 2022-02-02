564677a6c1d997d033dcb3ccf02df687a501c447
Teddy Sahr <tedward1218@gmail.com>
2022-02-01 19:06:19 -0500

    basic game structure layout in js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const chosenWordDisplay = document.getElementById("objective-words");
 const startButton = document.getElementById("start-button");
+const inputButton = document.getElementById("input-button");
+const inputText = document.getElementById("input-text");
+const inputWords = document.getElementById("input-words");
 
 const wordList = [
   ["ROBOT", "WINCE"],
@@ -8,6 +11,49 @@ const wordList = [
 
 function runGame() {
   chosenWordDisplay.textContent = getWords();
+
+  //start timer
+}
+
+let currentWord = wordList[0][0];
+
+function addWord() {
+  let count = 0;
+
+  const compareWordObj = {};
+
+  const currentWordArray = currentWord.split("");
+
+  for (letter of currentWordArray) {
+    compareWordObj[letter] = true;
+  }
+
+  const potentialWordArray = inputText.value.split("");
+
+  for (letter of potentialWordArray) {
+    if (letter in compareWordObj) {
+      count++;
+    }
+  }
+
+  if (count >= 2) {
+    const newWord = document.createElement("li");
+    newWord.textContent = inputText.value;
+    inputWords.appendChild(newWord);
+    currentWord = inputText.value;
+    count = 0;
+  } else {
+    alert(
+      "words needs to contain at least two letters from the previous words!"
+    );
+    count = 0;
+  }
+
+  console.log(compareWordObj);
+
+  //   const enteredWord = inputText.textContent;
+
+  console.log(inputText.value);
 }
 
 function getWords() {
@@ -15,3 +61,4 @@ function getWords() {
 }
 
 startButton.addEventListener("click", runGame);
+inputButton.addEventListener("click", addWord);
