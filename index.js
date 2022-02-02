59b4b620b07ee93c59a218edb3e3d3d5e6218170
Teddy Sahr <tedward1218@gmail.com>
2022-02-02 01:55:38 -0500

    added ability to type out a word and add it to the list

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const startButton = document.getElementById("start-button");
 const inputButton = document.getElementById("input-button");
 const inputText = document.getElementById("input-text");
 const inputWords = document.getElementById("input-words");
+const wordBox = document.getElementById("word-box");
 
 const wordList = [
   ["ROBOT", "WINCE"],
@@ -15,10 +16,12 @@ function runGame() {
   //start timer
 }
 
-let currentWord = wordList[0][0];
+let currentWord = wordList[1][0];
 
 function addWord() {
   let count = 0;
+
+  console.log(wordArray);
 
   const compareWordObj = {};
 
@@ -28,19 +31,17 @@ function addWord() {
     compareWordObj[letter] = true;
   }
 
-  const potentialWordArray = inputText.value.split("");
-
-  for (letter of potentialWordArray) {
+  for (letter of wordArray) {
     if (letter in compareWordObj) {
       count++;
     }
   }
 
-  if (count >= 2) {
+  if (count >= 3) {
     const newWord = document.createElement("li");
-    newWord.textContent = inputText.value;
+    newWord.textContent = wordArray.join("");
     inputWords.appendChild(newWord);
-    currentWord = inputText.value;
+    currentWord = wordArray.join("");
     count = 0;
   } else {
     alert(
@@ -50,15 +51,74 @@ function addWord() {
   }
 
   console.log(compareWordObj);
-
-  //   const enteredWord = inputText.textContent;
-
-  console.log(inputText.value);
 }
 
 function getWords() {
-  return wordList[0];
+  return wordList[1];
+}
+
+const wordArray = [];
+
+function typeWord(e) {
+  const key = e.key;
+
+  console.log(key.toString());
+
+  const keyString = key.toString();
+
+  const keyStringUpperCase = keyString.toUpperCase();
+
+  console.log(wordArray);
+
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  for (letter of alphabet) {
+    if (keyStringUpperCase === letter && wordArray.length < 5) {
+      wordArray.push(keyStringUpperCase);
+    }
+  }
+
+  if (key === "Backspace") {
+    wordArray.pop();
+  }
+
+  if (key === "Enter") {
+    addWord();
+    wordArray.splice(0, wordArray.length);
+  }
+
+  wordBox.textContent = wordArray.join("");
 }
 
 startButton.addEventListener("click", runGame);
+
 inputButton.addEventListener("click", addWord);
+
+document.addEventListener("keydown", typeWord);
