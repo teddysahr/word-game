68709484e3d96c560838011471d8243bdced9b63
Teddy Sahr <tedward1218@gmail.com>
2022-02-03 00:08:08 -0500

    fixed input field for mobile

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const chosenWordDisplay = document.getElementById("objective-words");
 const startButton = document.getElementById("start-button");
 const inputButton = document.getElementById("input-button");
 const inputWords = document.getElementById("input-words");
+const word = document.getElementById("word");
+const inputText = document.getElementById("input-text");
 const wordBox = document.getElementById("word-box");
 
 const wordList = [
@@ -10,40 +12,54 @@ const wordList = [
 ];
 
 function runGame() {
-  chosenWordDisplay.textContent = getWords();
+  const gameWords = getWords();
+
+  chosenWordDisplay.textContent = gameWords;
+
+  currentWord = gameWords[0];
+
+  console.log(currentWord);
 
   //start timer
 }
 
-let currentWord = wordList[1][0];
+let currentWord;
 
 function addWord() {
   let count = 0;
+
+  const upperCase = inputText.value.toUpperCase();
+
+  const addWordArray = upperCase.split("");
 
   const currentWordArray = currentWord.split("");
 
   const compareWordObj = {};
 
-  console.log(wordArray);
+  console.log(currentWordArray);
 
   for (letter of currentWordArray) {
     compareWordObj[letter] = true;
   }
 
-  for (letter of wordArray) {
+  for (letter of addWordArray) {
     if (letter in compareWordObj) {
       count++;
     }
   }
 
   if (count >= 3) {
-    const newWord = document.createElement("li");
-    newWord.textContent = wordArray.join("");
-    inputWords.appendChild(newWord);
-    currentWord = wordArray.join("");
+    const newWordDiv = document.createElement("div");
+    wordBox.appendChild(newWordDiv);
+    for (let i = 0; i < addWordArray.length; i++) {
+      const newLetter = document.createElement("div");
+      newLetter.textContent = addWordArray[i];
+      newWordDiv.appendChild(newLetter);
+    }
+    currentWord = addWordArray.join("");
     count = 0;
-    wordArray.splice(0, wordArray.length);
     word.innerHTML = "";
+    inputText.value = "";
   } else {
     alert(
       "words needs to contain at least two letters from the previous words!"
@@ -55,72 +71,27 @@ function addWord() {
 }
 
 function getWords() {
-  return wordList[1];
+  return wordList[Math.floor(Math.random() * wordList.length)];
 }
 
-const wordArray = [];
-
-function typeWord(e) {
-  const key = e.key;
-
-  console.log(key.toString());
-
-  const keyString = key.toString();
-
-  const keyStringUpperCase = keyString.toUpperCase();
-
-  const alphabet = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-
-  for (letter of alphabet) {
-    if (keyStringUpperCase === letter && wordArray.length < 5) {
-      wordArray.push(keyStringUpperCase);
-      const newLetter = document.createElement("div");
-      newLetter.textContent = keyStringUpperCase;
-      word.appendChild(newLetter);
-    }
+function displayLetter() {
+  word.innerHTML = "";
+  const inputArray = inputText.value.split("");
+  for (let i = 0; i < inputArray.length; i++) {
+    const newLetter = document.createElement("div");
+    newLetter.textContent = inputArray[i].toUpperCase();
+    word.appendChild(newLetter);
   }
-
-  if (key === "Backspace") {
-    wordArray.pop();
-    word.removeChild(word.lastElementChild);
-  }
-
-  if (key === "Enter") {
-    addWord();
-  }
-
-  wordBox.textContent = wordArray.join("");
 }
 
 startButton.addEventListener("click", runGame);
 
 inputButton.addEventListener("click", addWord);
 
-document.addEventListener("keydown", typeWord);
+inputText.addEventListener("keyup", displayLetter);
+
+inputText.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    inputButton.click();
+  }
+});
